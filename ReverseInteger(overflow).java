0c79bdbe7b6cca411716f70c01fa326dbbc4c8b2
Alicia <tl2180@nyu.edu>
2019-03-14 20:40:14 -0400

    3.14-4

---
+++ ReverseInteger(overflow).java
@@ -0,0 +1,30 @@
+public int reverse(int x)
+{
+    int result = 0;
+
+    while (x != 0)
+    {
+        int tail = x % 10;
+        int newResult = result * 10 + tail;
+        if ((newResult - tail) / 10 != result)
+        { return 0; }
+        result = newResult;
+        x = x / 10;
+    }
+
+    return result;
+}
+
+
+public int reverse(int x) {
+        int prevRev = 0 , rev= 0;
+        while( x != 0){
+            rev= rev*10 + x % 10;
+            if((rev - x % 10) / 10 != prevRev){
+                return 0;
+            }
+            prevRev = rev;
+            x= x/10;
+        }
+        return rev;
+    }
