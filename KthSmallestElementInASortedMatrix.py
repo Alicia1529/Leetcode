af68d883e74dcb1044e7ce1c20615c6fe1039c17
Alicia <tl2180@nyu.edu>
2019-03-25 17:03:00 -0400

    3.25

---
+++ KthSmallestElementInASortedMatrix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        return sorted([i for j in matrix for i in j])[k-1]
